6914985370ee423c50bb20b9decbc03743b29f54
Pragun Bhutani <pragun.bhutani897@gmail.com>
2024-01-18 00:47:12 +0530

    db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
- 
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants"
+import express from "express"
+const app=express();
+
+
+
+
+
+
+
+
+
+
+// 1st approach
+
+/*
+// function connectDB(){}
+// connectDB()
+
+;(async ()=>{
+    try{
+        await mongoose.connect('${process.env.MONGODV_URI}/${DB_NAME}'); // db connect
+        app.on("error",(error)=>{   // listeners erro on express app
+            console.log("ERR:",error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log('App is listening on port $ {process.env.PORT}');
+        })
+    }catch(error){
+        console.log("ERROR:",error);
+        throw err;
+    }
+})()
+
+*/
